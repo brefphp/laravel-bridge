197c3256814504bd0e15f8780d8aab2e4413a80e
Matthieu Napoli <matthieu@mnapoli.fr>
2020-06-11 11:30:16 +0200

    Add a BrefServiceProvider

---
+++ src/BrefServiceProvider.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace Bref\LaravelBridge;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+use RuntimeException;
+
+class BrefServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // Make sure the directory for compiled views exist
+        $compiledViewDirectory = Config::get('view.compiled');
+        if (! is_dir($compiledViewDirectory)) {
+            // The directory doesn't exist: let's create it, else Laravel will not create it automatically
+            // and will fail with an error
+            if (! mkdir($compiledViewDirectory, 0755, true) && ! is_dir($compiledViewDirectory)) {
+                throw new RuntimeException(sprintf('Directory "%s" cannot be created', $compiledViewDirectory));
+            }
+        }
+    }
+
+    public function register(): void
+    {
+        $isRunningInLambda = isset($_SERVER['LAMBDA_TASK_ROOT']);
+
+        // Laravel Mix URL for assets stored on S3
+        $mixAssetUrl = $_SERVER['MIX_ASSET_URL'];
+        if ($mixAssetUrl) {
+            Config::set('app.mix_url', $mixAssetUrl);
+        }
+
+        // The rest below is specific to AWS Lambda
+        if (! $isRunningInLambda) {
+            return;
+        }
+
+        // We change Laravel's default log destination to stderr
+        // (if it hasn't been changed by the user)
+        $logDriver = Config::get('logging.default');
+        if ($logDriver === 'stack' && ! isset($_SERVER['LOG_CHANNEL'])) {
+            Config::set('logging.default', 'stderr');
+        }
+
+        // Store compiled views in `/tmp` because they are generated at runtime
+        // and `/tmp` is the only writable directory on Lambda
+        Config::set('view.compiled', '/tmp/storage/framework/views');
+
+        // Allow all proxies because AWS Lambda runs behind API Gateway
+        Config::set('trustedproxy.proxies', '*');
+
+        // Sessions cannot be stored to files, so we use cookies by default instead
+        $sessionDriver = Config::get('session.driver');
+        if ($sessionDriver === 'file') {
+            Config::set('session.driver', 'cookie');
+        }
+    }
+}
