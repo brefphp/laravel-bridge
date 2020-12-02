4f97d57ccc55bf9602275ff19460d6d600d30a5a
George Boot <george@famboot.nl>
2020-12-02 12:58:36 +0100

    refactor

--- src/Queue/Worker.php
+++ src/Queue/Worker.php
@@ -10,18 +10,6 @@ class Worker extends LaravelWorker
 {
     public function runSqsJob(Job $job, string $connectionName, WorkerOptions $options): void
     {
-        pcntl_async_signals(true);
-
-        // pcntl_signal(SIGALRM, function () use ($job) {
-        //     throw new VaporJobTimedOutException($job->resolveName());
-        // });
-
-        pcntl_alarm(
-            max($this->timeoutForJob($job, $options), 0)
-        );
-
         $this->runJob($job, $connectionName, $options);
-
-        pcntl_alarm(0);
     }
 }
