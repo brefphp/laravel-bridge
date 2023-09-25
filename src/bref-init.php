5ff0cfca69a3328ec120181990039b8244c115d9
Julian Hundeloh <5358638+jaulz@users.noreply.github.com>
2023-09-25 07:35:44 +0200

    feat: add environment variable to override Laravel home directory

--- src/bref-init.php
+++ src/bref-init.php
@@ -7,7 +7,7 @@ use Bref\LaravelBridge\MaintenanceMode;
 use Bref\LaravelBridge\StorageDirectories;
 
 Bref::beforeStartup(static function () {
-    $laravelHome = __DIR__ . '/../../../../';
+    $laravelHome = $_ENV['LARAVEL_HOME'] ?? __DIR__ . '/../../../../';
 
     if (! defined('STDERR')) {
         define('STDERR', fopen('php://stderr', 'wb'));
